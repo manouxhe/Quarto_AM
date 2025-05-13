0c5414a74eb3a32d57b78f6907bafc7342f6b56e
manouxhe <23346@ecam.be>
2025-05-13 23:15:36 +0200

    Ajout d'un fichier test

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from client import play_move, all_pieces  # si ton code est dans client.py (sinon remplace par le bon nom)
+from client import play_move, all_pieces  
 
 # cree un plateau vide (aucune pièce posée)
 plateau_test = [None] * 16
@@ -6,16 +6,13 @@ plateau_test = [None] * 16
 # choisis  pièce exis a tester 
 piece_reçue = "BDCP"  
 
-# Simule etat envoye par le serveur
 etat_simulé = {
     "board": plateau_test,
     "piece": piece_reçue
 }
 
-# Appelle la fonction de ton IA
 coup = play_move(etat_simulé)
 
-# Affiche le résultat
 print("Coup joué par IA :")
 print(" Position jouée :", coup["pos"])
 print(" Piece donnée :", coup["piece"])
